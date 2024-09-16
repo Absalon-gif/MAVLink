0be8dbbd5f090bc730a694019abe38003dfe29b5
Absalon-gif <abbsalon2000@gmail.com>
2024-09-16 19:13:09 +0200

    Added encryption schemes and modified UI

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import threading
 from PyQt5.QtCore import pyqtSignal, QObject
 from PyQt5.QtWidgets import QApplication, QLineEdit, QMainWindow, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel, \
-    QMessageBox
+    QMessageBox, QCheckBox, QHBoxLayout
 from src.MAVLink.mav_connection import MAVLinkSocket, MAVLinkRadioCommunicator, AccessControl
 from src.MAVLink.mav_message import MAVLinkMessageCreator
 
@@ -46,6 +46,7 @@ class GCSApp(QMainWindow):
         self.mav_communicator = MAVLinkRadioCommunicator(mav_socket)
         self.listen_broadcast = True
         self.listen_thread = None
+        self.encrypted = None
 
         # Set up signals
         self.signals = WorkerSignals()
@@ -68,28 +69,55 @@ class GCSApp(QMainWindow):
         self.title_label.setStyleSheet("font-size: 16pt; font-weight: bold;")
         layout.addWidget(self.title_label)
 
+        # Drone SSID Input
         self.ssid_input = QLineEdit(self)
         self.ssid_input.setPlaceholderText("Enter Drone's SSID")
         layout.addWidget(self.ssid_input)
 
+        # Buttons
         self.connect_button = QPushButton('Connect to Drone', self)
         self.connect_button.clicked.connect(self.connect_to_drone)
         layout.addWidget(self.connect_button)
 
-        self.connect_button = QPushButton('Receive Broadcast', self)
-        self.connect_button.clicked.connect(self.start_listening_thread)
-        layout.addWidget(self.connect_button)
+        self.receive_button = QPushButton('Receive Broadcast', self)
+        self.receive_button.clicked.connect(self.start_listening_thread)
+        layout.addWidget(self.receive_button)
 
+        # Drone Info Label
         self.drone_info_label = QLabel('Drone not discovered yet', self)
         layout.addWidget(self.drone_info_label)
 
+        # Heartbeat Buttons
         self.send_heartbeat_button = QPushButton('Send Heartbeat Message', self)
         self.send_heartbeat_button.clicked.connect(self.send_heartbeat)
         layout.addWidget(self.send_heartbeat_button)
 
+        self.send_multiple_heartbeat_button = QPushButton('Send Multiple Heartbeats', self)
+        self.send_multiple_heartbeat_button.clicked.connect(self.send_multiple_heartbeats)
+        layout.addWidget(self.send_multiple_heartbeat_button)
+
+        self.iterations_input = QLineEdit(self)
+        self.iterations_input.setPlaceholderText("Enter number of iterations")
+        layout.addWidget(self.iterations_input)
+
+        # Encrypted/Unencrypted Option Flag
+        self.encryption_checkbox = QCheckBox("Send Encrypted", self)
+        layout.addWidget(self.encryption_checkbox)
+
+        # Horizontal layout for logs and time display
+        h_layout = QHBoxLayout()
+
+        # Communication Logs Textbox
         self.logs_text = QTextEdit(self)
         self.logs_text.setReadOnly(True)
-        layout.addWidget(self.logs_text)
+        h_layout.addWidget(self.logs_text)
+
+        # Average Time Textbox
+        self.time_text = QTextEdit(self)
+        self.time_text.setReadOnly(True)
+        h_layout.addWidget(self.time_text)
+
+        layout.addLayout(h_layout)
 
         self.show()
 
@@ -130,6 +158,7 @@ class GCSApp(QMainWindow):
                         self.mav_socket.drone_ip = drone_ip
                         self.mav_socket.drone_port = drone_port
                         self.mav_socket.host = MAVLinkSocket.get_ipaddr()
+                        self.is_encrypted()
                         print(f"Discovered drone at {drone_ip}:{drone_port}, with SYS_ID: {data}")
                         self.send_heartbeat()
                         self.signals.log.emit(f"Discovered drone at {drone_ip}:{drone_port}, with SYS_ID: {data}")
@@ -137,6 +166,17 @@ class GCSApp(QMainWindow):
                         self.listen_broadcast = False
             except Exception as e:
                 self.signals.log.emit(f"Error receiving datagram: {e}")
+
+    def is_encrypted(self):
+        self.encrypted = self.encryption_checkbox.isChecked()
+        if self.encrypted:
+            # Generate the static key to share
+            MAVLinkSocket.generate_key()
+            # Share the static key with the Drone
+            self.mav_communicator.share_static_key(self.mav_socket.drone_ip, self.mav_socket.drone_port)
+            return True
+        else:
+            return False
 
     def update_drone_info(self, drone_ip, drone_port, data):
         print(f"Updating drone info: {drone_ip}:{drone_port}, with SYS_ID: {data}")
@@ -147,12 +187,45 @@ class GCSApp(QMainWindow):
     def send_heartbeat(self):
         mav_message = MAVLinkMessageCreator().create_message(0)
         try:
-            self.mav_communicator.send_message(mav_message, heartbeat_values,
-                                               self.mav_socket.drone_ip, self.mav_socket.drone_port)
+            if self.encrypted:
+                self.mav_communicator.send_message(mav_message, heartbeat_values,
+                                                   self.mav_socket.drone_ip, self.mav_socket.drone_port, True)
+            else:
+                self.mav_communicator.send_message(mav_message, heartbeat_values,
+                                                   self.mav_socket.drone_ip, self.mav_socket.drone_port, False)
             self.signals.log.emit("Heartbeat message sent")
             self.signals.heartbeat_sent.emit()
         except Exception as e:
             self.signals.log.emit(f"Failed to send heartbeat message: {e}")
+
+    def send_multiple_heartbeats(self):
+        iterations = int(self.iterations_input.text())
+        start_time = time.perf_counter()
+        mav_message = MAVLinkMessageCreator().create_message(0)
+        for _ in range(iterations):
+            try:
+                if self.encrypted:
+                    self.mav_communicator.send_message(mav_message, heartbeat_values,
+                                                       self.mav_socket.drone_ip, self.mav_socket.drone_port, True)
+                else:
+                    self.mav_communicator.send_message(mav_message, heartbeat_values,
+                                                       self.mav_socket.drone_ip, self.mav_socket.drone_port, False)
+            except Exception as e:
+                self.time_text.append(f"Failed to send heartbeat message: {e}")
+
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        avg_time_per_packet = total_time / iterations
+        if self.encrypted:
+            print(f"Total time for {iterations} encrypted packets: {total_time:.6f} seconds")
+            self.time_text.append(f"Total time for {iterations} encrypted packets: {total_time:.6f} seconds")
+            print(f"Average time per encrypted packet: {avg_time_per_packet:.6f} seconds")
+            self.time_text.append(f"Average time per encrypted packet: {avg_time_per_packet:.6f} seconds")
+        else:
+            print(f"Total time for {iterations} unencrypted packets: {total_time:.6f} seconds")
+            self.time_text.append(f"Total time for {iterations} unencrypted packets: {total_time:.6f} seconds")
+            print(f"Average time per unencrypted packet: {avg_time_per_packet:.6f} seconds")
+            self.time_text.append(f"Average time per unencrypted packet: {avg_time_per_packet:.6f} seconds")
 
     def handle_heartbeat_sent(self):
         QMessageBox.information(self, "Info", "Heartbeat message sent")
--- src/MAVLink/mav_connection.py
+++ src/MAVLink/mav_connection.py
@@ -1,9 +1,10 @@
 import socket
 import struct
-from typing import Optional
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
 
 from .mav_message import MAVLinkMessage, MAVLinkSerializer, MAVLinkChecksum, MAVLinkMessageCreator
-
 
 # MAVLink constants
 START_BYTE = 0xFE
@@ -17,13 +18,15 @@ CRC_EXTRA_CONSTANTS = {
     0: 50  # HEARTBEAT
 }
 
+STATIC_KEY = None
+
 
 class MAVLinkSocket:
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_socket.bind((self.host, self.port))  # When connecting to PI ~ socket.bind(("en0", self.port))
+        self.udp_socket.bind((self.host, self.port))  # When connecting to PI - socket.bind(("en0", self.port))
         self.drone_ip = None
         self.drone_port = None
         self.access_control = AccessControl()
@@ -68,6 +71,12 @@ class MAVLinkSocket:
         hostname = socket.gethostname()
         return socket.gethostbyname(hostname)
 
+    @staticmethod
+    def generate_key():
+        global STATIC_KEY
+        key = os.urandom(32)
+        STATIC_KEY = key
+
     def parse_system_id(self, data: bytes):
         if len(data) < 8:
             sys_id = self.parse_broadcast_id(data)
@@ -107,12 +116,19 @@ class MAVLinkRadioCommunicator:
                 print("Message ID not recognized")
                 return
 
+            received_payload = data[6:6 + length]
+            # Decrypt message
+            if length > 10:  # if length is greater than 10 for now, it means that the message is encrypted
+                decryptor = MAVLinkSec(STATIC_KEY)
+                received_payload = decryptor.decrypt_chacha20(data[6:6 + length])
+
             serializer = MAVLinkSerializer(mav_message)
-            payload = serializer.deserialize(data[6:6 + length])
+            payload = serializer.deserialize(received_payload)
             received_checksum = data[6 + length]
 
             crc_extra = CRC_EXTRA_CONSTANTS.get(msg_id, 0)
-            computed_checksum = MAVLinkChecksum.compute(data[1:6 + length], crc_extra)
+            checksum_data = data[1:6 + length]
+            computed_checksum = MAVLinkChecksum().compute(checksum_data, crc_extra)
 
             if received_checksum == computed_checksum:
                 print(f"Received packet: SYS: {system_id}, COMP: {component_id}, LEN: {length}, MSG ID: {msg_id}, "
@@ -125,26 +141,42 @@ class MAVLinkRadioCommunicator:
             print("Invalid MAVLink message start byte")
             return None
 
-    def send_message(self, message: MAVLinkMessage, values: dict, target_host, target_port) -> None:
+    def share_static_key(self, target_host, target_port):
+        identifier = b'\x01'
+        preshared_key = identifier + STATIC_KEY
+        self.mavlink_socket.send_datagram(preshared_key, target_host, target_port)
+        print(f"Share static key: {preshared_key}")
+
+    def send_message(self, message: MAVLinkMessage, values: dict, target_host, target_port, encrypted) -> None:
         serializer = MAVLinkSerializer(message)
         payload = serializer.serialize(values)
 
-        # packet creation
-        length = len(payload)
+        mav_message = payload
+
+        if encrypted:
+            # Encrypt message
+            encryptor = MAVLinkSec(STATIC_KEY)
+            mav_message = encryptor.encrypt_chacha20(payload)
+
+        # Packet creation
+        length = len(mav_message)
         msg_id = message.message_id
         header = struct.pack('<BBBBBB', START_BYTE, length, SEQUENCE, SYSTEM_ID, COMPONENT_ID, msg_id)
 
         # Compute Checksum
         crc_extra = CRC_EXTRA_CONSTANTS.get(msg_id, 0)
-        checksum = MAVLinkChecksum.compute(header[1:] + payload, crc_extra)
+        checksum = MAVLinkChecksum().compute(header[1:] + mav_message, crc_extra)
         checksum_bytes = struct.pack('<H', checksum)
 
-        mavlink_packet = header + payload + checksum_bytes
+        mavlink_packet = header + mav_message + checksum_bytes
 
         print("Bytes Sent:", len(mavlink_packet))
 
         hex_string = ' '.join(format(byte, '02x') for byte in mavlink_packet)
         print("Datagram:", hex_string)
+
+        print(f"Sent packet: SYS: {header[3]}, COMP: {header[4]}, LEN: {length}, MSG ID: {msg_id}, "
+              f"Payload: {payload}")
 
         self.mavlink_socket.send_datagram(mavlink_packet, target_host, target_port)
 
@@ -166,6 +198,49 @@ class AccessControl:
 
 
 class MAVLinkSec:
-    def __init__(self):
-        self.algo = None
+    def __init__(self, key=STATIC_KEY):
+        self.key = key
 
+    def encrypt_chacha20(self, payload):
+        nonce = os.urandom(16)
+
+        algorithm = algorithms.ChaCha20(self.key, nonce)
+        cipher = Cipher(algorithm, mode=None, backend=default_backend())
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(payload)
+
+        return nonce + ciphertext
+
+    def decrypt_chacha20(self, encrypted_message):
+        nonce = encrypted_message[:16]
+        ciphertext = encrypted_message[16:]
+
+        algorithm = algorithms.ChaCha20(self.key, nonce)
+        cipher = Cipher(algorithm, mode=None, backend=default_backend())
+        decryptor = cipher.decryptor()
+        decrypted_message = decryptor.update(ciphertext)
+
+        return decrypted_message
+
+    def aes_encrypt(self, payload):
+        nonce = os.urandom(16)
+
+        algorithm = algorithms.AES(self.key)
+        cipher = Cipher(algorithm, modes.CTR(nonce), backend=default_backend())
+
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(payload) + encryptor.finalize()
+
+        return nonce + ciphertext
+
+    def aes_decrypt(self, cipher_message):
+        nonce = cipher_message[:16]
+        encrypted_message = cipher_message[16:]
+
+        algorithm = algorithms.AES(self.key)
+        cipher = Cipher(algorithm, modes.CTR(nonce), backend=default_backend())
+
+        decrypt = cipher.decryptor()
+        decrypted_message = decrypt.update(encrypted_message) + decrypt.finalize()
+
+        return decrypted_message
